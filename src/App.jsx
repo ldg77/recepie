5303e3a12eb3774cf8dac94692f261f597f857bd
Uwe Kremer <uwekremer1971@gmx.de>
2022-12-07 12:07:19 +0100

    navbar weiter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
-import { Route, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Recepie from "./components/bottom/Recepie.jsx";
 import Recepies from "./components/bottom/Recepies.jsx";
 import Nav from "./components/top/Nav.jsx";
+import Form from './components/bottom/Form.jsx';
+
 function App() {
   const [tag, setTag] = useState("");
   return (
     <div className="App">
       <Nav setTag={setTag} />
-      <Router>
+      <Routes>
         <Route path="/" element={<Recepies tag={tag} />} />
         <Route path="/:id" element={<Recepie />} />
         <Route path="/form" element={<Form />} />
-      </Router>
+      </Routes>
     </div>
   );
 }
--- src/components/top/Nav.jsx
+++ src/components/top/Nav.jsx
@@ -1,4 +1,8 @@
 
+const tags = [
+  "Beef", "Pork", "Chicken", "Vegan", "Vegetarian", "FastFood",
+]
+
 const Nav = ({ setTag }) => {
   return (
     <div>
@@ -6,7 +10,9 @@ const Nav = ({ setTag }) => {
         <div className="logo">Logo</div>
         <div className="nav-btn">Home</div>
         <div className="nav-btn">Add Recipe</div>
-        <div className="nav-dropdown">Filter Ingredients</div>
+        <div className="nav-dropdown">
+          <select onChange={(event) => setTag(prev => prev=event.target.value)}>{tags.map((tag) => <option value={tag} key={tag}>{tag}</option>)}</select>
+        </div>
       </div>
     </div>
   )
