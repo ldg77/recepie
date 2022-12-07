c84f5f91b2f6e22195b2033623bc9d5d1be568ea
Uwe Kremer <uwekremer1971@gmx.de>
2022-12-07 14:02:10 +0100

    navbar gestylt

--- src/components/top/Nav.jsx
+++ src/components/top/Nav.jsx
@@ -1,17 +1,32 @@
+import './nav.scss';
+import { useNavigate } from 'react-router-dom';
 
 const tags = [
-  "Beef", "Pork", "Chicken", "Vegan", "Vegetarian", "FastFood",
-]
+  "", "Beef", "Pork", "Chicken", "Vegan", "Vegetarian", "FastFood",
+];
+
 
 const Nav = ({ setTag }) => {
+  
+  const navigator = useNavigate();
+
   return (
+    
     <div>
       <div className="navbar">
-        <div className="logo">Logo</div>
-        <div className="nav-btn">Home</div>
-        <div className="nav-btn">Add Recipe</div>
+        <img src="" alt="Logo" className="logo"/>
+        <button className="nav-btn" onClick={() => {
+          navigator("/")
+        }}>Home</button>
+        <button className="nav-btn" onClick={() => {
+          navigator("/form")
+        }}>Add Recipe</button>
         <div className="nav-dropdown">
-          <select onChange={(event) => setTag(prev => prev=event.target.value)}>{tags.map((tag) => <option value={tag} key={tag}>{tag}</option>)}</select>
+          <label className="navbar-label" htmlFor="select">Select Ingredients </label>
+          <select id="select "onChange={(event) => setTag(prev => prev=event.target.value)}>
+            {tags.map((tag) => <option value={tag} key={tag}>{tag}</option>)}
+          
+          </select>
         </div>
       </div>
     </div>
