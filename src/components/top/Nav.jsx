fd71f8f18d830d30f8becf968b8d716ab1f66dd8
Uwe Kremer <uwekremer1971@gmx.de>
2022-12-07 11:31:17 +0100

    navbar angelegt

--- src/components/top/Nav.jsx
+++ src/components/top/Nav.jsx
@@ -1,5 +1,15 @@
+
 const Nav = ({ setTag }) => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <div className="navbar">
+        <div className="logo">Logo</div>
+        <div className="nav-btn">Home</div>
+        <div className="nav-btn">Add Recipe</div>
+        <div className="nav-dropdown">Filter Ingredients</div>
+      </div>
+    </div>
+  )
 };
 
 export default Nav;
